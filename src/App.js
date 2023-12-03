23aa5b4b7541909cd2789659364868b6c1e2c839
solomonsanthosh <solosanty2064@gmail.com>
2023-12-03 16:23:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+import Navigation from './navigation';
+import {store} from './store';
+import {Provider} from 'react-redux';
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
+  );
+}
